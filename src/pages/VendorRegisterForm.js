b83f311c03835e1df2317af0de53339b5ad4f8e7
yared <fdessalew@gmail.com>
2023-09-21 12:58:26 +0300

    register form is add

--- src/pages/VendorRegisterForm.js
+++ src/pages/VendorRegisterForm.js
@@ -1,57 +1,100 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import '../pages/forms.css';
 
 const VendorRegisterForm = () => {
   const [companyName, setCompanyName] = useState('');
   const [phone, setPhone] = useState('');
   const [email, setEmail] = useState('');
+  const [isRegistered, setIsRegistered] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const handleFormSubmit = (e) => {
+  useEffect(() => {
+    // Check if the user is already registered when the component mounts
+    checkRegistrationStatus();
+  }, []);
+
+  const checkRegistrationStatus = async () => {
+    try {
+      setIsLoading(true);
+      const response = await axios.get(`https://neari.marketing/api/vendor/${0}`, {
+        // Add any necessary headers or parameters for your API request
+      });
+      if (response.status === 200) {
+        setIsRegistered(true);
+      }
+      setIsLoading(false);
+    } catch (error) {
+      console.log('Error checking registration status:', error);
+      setIsLoading(false);
+    }
+  };
+
+  const handleFormSubmit = async (e) => {
     e.preventDefault();
-
     // Perform vendor registration logic here
     // You can access the entered values in the state variables (companyName, phone, email)
 
-    // Reset the form fields
-    setCompanyName('');
-    setPhone('');
-    setEmail('');
+    try {
+      setIsLoading(true);
+      // Make your API request to register the vendor
+      // Example using axios:
+      const response = await axios.post('https://negari.marketing/api/vendor/', {
+        companyName,
+        phone,
+        email,
+      });
+      setIsRegistered(true);
+      setIsLoading(false);
+      console.log('Registration successful:', response.data);
+    } catch (error) {
+      console.log('Error registering vendor:', error);
+      setIsLoading(false);
+    }
   };
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (isRegistered) {
+    return (
+      <div>
+        <h2>Already Registered</h2>
+        <p>You have already registered as a vendor.</p>
+      </div>
+    );
+  }
 
   return (
     <form onSubmit={handleFormSubmit}>
-      <div className="mb-3">
-        <label htmlFor="companyName" className="form-label">Company Name:</label>
+      <label>
+        Company Name:
         <input
           type="text"
-          id="companyName"
-          className="form-control"
           value={companyName}
           onChange={(e) => setCompanyName(e.target.value)}
         />
-      </div>
-
-      <div className="mb-3">
-        <label htmlFor="phone" className="form-label">Phone:</label>
+      </label>
+      <br />
+      <label>
+        Phone:
         <input
-          type="tel"
-          id="phone"
-          className="form-control"
+          type="text"
           value={phone}
           onChange={(e) => setPhone(e.target.value)}
         />
-      </div>
-
-      <div className="mb-3">
-        <label htmlFor="email" className="form-label">Email:</label>
+      </label>
+      <br />
+      <label>
+        Email:
         <input
-          type="email"
-          id="email"
-          className="form-control"
+          type="text"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
-      </div>
-
+      </label>
+      <br />
       <button type="submit" className="btn btn-primary">Register</button>
     </form>
   );
